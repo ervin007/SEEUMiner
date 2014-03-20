1a59bed11691bb8342228933c8460e4968a2be97
vshehu <v.shehu@seeu.edu.mk>
2014-03-20 10:39:41 +0100

    Statistics class

---
+++ SEEUMiner.Library/Statistics.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SEEUMiner.Library
+{
+    public class Statistics
+    {
+        double[] array_items;
+
+        public Statistics(double[] items)
+        {
+            array_items = items;
+            
+        }
+        public double Min()
+        {
+            return 0;
+        }
+
+        public double Max()
+        {
+            return 0;
+        }
+
+        public double Average()
+        {
+            return 0;
+        }
+
+        public double Median()
+        {
+            return 0;
+        }
+
+        public double Percentile(int n)
+        {
+            return 0;
+        }
+
+        public double StandardDeviationP()
+        {
+            return 0;
+        }
+
+        public double StandardDeviationS()
+        {
+            return 0;
+        }
+
+        
+    }
+}
