10b0954f81b701745c0c3f46a0457079c7f60217
vshehu <v.shehu@seeu.edu.mk>
2014-04-03 11:44:05 +0200

    implemented cosine similarity

--- SEEUMiner.App/Form1.Designer.cs
+++ SEEUMiner.App/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.lstNotNormalized = new System.Windows.Forms.ListBox();
             this.lstNormalized = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // txtItems
@@ -89,11 +90,22 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(149, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(609, 261);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.lstNormalized);
             this.Controls.Add(this.lstNotNormalized);
@@ -113,6 +125,7 @@
         private System.Windows.Forms.ListBox lstNotNormalized;
         private System.Windows.Forms.ListBox lstNormalized;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
